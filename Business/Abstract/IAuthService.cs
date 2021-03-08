ed633ff11c2a28efa76fcdac5547d78b99920004
ayglcinar <60033267+ayglcinar@users.noreply.github.com>
2021-03-08 15:07:44 +0300

    Add files via upload

---
+++ Business/Abstract/IAuthService.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using Core.Entities.Concrete;
+using Core.Utilities.Results;
+using Core.Utilities.Security.JWT;
+using Entities.DTOs;
+
+namespace Business.Abstract
+{
+    public interface IAuthService
+    {
+        public IDataResult<User> Register(UserForRegisterDto userForRegisterDto, string password);
+        public IDataResult<User> Login(UserForLoginDto userForLoginDto);
+        public IResult UserExists(string email);
+        public IDataResult<AccessToken> CreateAccessToken(User user);
+    }
+}
---
+++ Business/Abstract/IUserService.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using Core.Entities.Concrete;
+using Core.Utilities.Results;
+using Entities.Concrete;
+using Entities.DTOs;
+
+namespace Business.Abstract
+{
+    public interface IUserService
+    {
+        //List<Car> GetAll();
+        //IDataResult<List<Core.Entities.Concrete.User>> GetAll();
+        IResult Add(Core.Entities.Concrete.User user);
+        //IResult Update(Core.Entities.Concrete.User user);
+        //IResult Delete(Core.Entities.Concrete.User user);
+
+        IDataResult<List<OperationClaim>> GetClaims(Core.Entities.Concrete.User user);
+        IDataResult<Core.Entities.Concrete.User> GetByMail(string email);
+    }
+}
