ae03e638ef52e47ef034637aefde906ec9d3d789
ayglcinar <60033267+ayglcinar@users.noreply.github.com>
2021-03-08 15:08:57 +0300

    Add files via upload

---
+++ DataAccess/Abstract/IUserDal.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using Core.DataAccess;
+using Core.Entities.Concrete;
+using Entities.Concrete;
+
+namespace DataAccess.Abstract
+{
+    public interface IUserDal : IEntityRepository<Core.Entities.Concrete.User>
+    {
+        List<OperationClaim> GetClaims(Core.Entities.Concrete.User user);
+    }
+}
